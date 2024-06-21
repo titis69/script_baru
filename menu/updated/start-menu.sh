6555f48f29efb5606b0d27374af69fea175f03ac
titis69 <117496052+titis69@users.noreply.github.com>
2024-06-22 01:18:50 +0700

    Update start-menu.sh

--- menu/updated/start-menu.sh
+++ menu/updated/start-menu.sh
@@ -130,5 +130,5 @@ fi
 # echo -e " Fail2ban                :$RED [Error] $NC"
 # fi
 echo -e "$BLUE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
-echo -e "$BRED----------- Mod By Br-----------"
+echo -e "$BRED----------- Mod By Br----------------"
 echo -e "$BLUE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
